981ada00658dba559db231c6fbeeebf4f755f4b7
ddpalacios <45171403+ddpalacios@users.noreply.github.com>
2019-10-04 10:43:16 -0500

    Selected features Split: 70% training 15% testing

--- dataframe.py
+++ dataframe.py
@@ -1,20 +1,23 @@
 import pandas as pd
-#Data view configuration
+
+# Data view configuration
 pd.set_option('display.width', 320)
 pd.set_option('display.max_columns', 20)
-#Class just to make things slightly easier to read and handle organize our code...
-class dataFrame():
+
+
+# Class just to make things slightly easier to read and handle organize our code...
+class DataFrame():
     def __init__(self, csv):
         self.df = pd.read_csv(csv)
 
-    #any methods we need for our data will be here
+    # any methods we need for our data will be here
     def see_info(self):
         print(self.df.info())
 
-    def head(self, peek):
+    def head(self, peek=5):
         print(self.df.head(peek))
 
-    def tail(self, peek):
+    def tail(self, peek=5):
         print(self.df.tail(peek))
 
     def drop(self, col):
@@ -24,4 +27,7 @@ class dataFrame():
     def map_features(self, map, col):
         self.df[col] = self.df[col].map(map)
 
-
+    def select_data_points(self, feat, targ):
+        X = self.df.iloc[:, :feat].values
+        y = self.df.iloc[:, targ].values
+        return X,y
--- main.py
+++ main.py
@@ -2,28 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from dataframe import dataFrame  #our class to view and manipulate our data
+from dataframe import DataFrame  # our class to view and manipulate our data
 
-#lets start by opening our dataset and see what columns (Features) we are dealing with...
-df = dataFrame('wineanalysis.csv')
+# lets start by opening our dataset and see what columns (Features) we are dealing with...
+df = DataFrame('wineanalysis.csv')
 data_frame = df.df
-#df.see_info() #We see an col named Unnamed... lets get rid of it...
+
+# We see an col named Unnamed... lets get rid of it...
 df.drop('Unnamed: 0')
 
-#We need to convert our class lable (type) into numerical values... this is part of preprocessing our data
-#There is no convienient function to do this, so we will be using mapping
+# We need to convert our class lable (type) into numerical values... this is part of preprocessing our data
+# There is no convenient function to do this, so we will be using mapping
 type_map = {
-    'red' :1,
-    'white':0
+    'red': 1,
+    'white': 0
 }
-
-#replaced Red types for 1 and white as 0
+# replaced Red types for 1 and white as 0
 df.map_features(type_map, 'type')
-df.see_info() #All of our data is now numerical
+df.head()  # All of our data is now numerical
+
+# Selecting feautures  + target
+X, y = df.select_data_points(12, 12)
+
+print("Data:\n", X)
+print("\nTarget", y)
 
 
+# splitting our features using scikit learn train-test-split method
+# 70 % assigned in X_train & y_train
+# 30 % assigned in X_test, y_test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.03, random_state=0, stratify=y)
 
-
-
-
-
+print("\n\n\nX_train:\n\n", X_train)
+print("\n\n\nX_test:\n\n", X_test)
