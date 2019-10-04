9a619e82e64c8e4618e9d36a0a37d625d5df2bd5
ddpalacios <45171403+ddpalacios@users.noreply.github.com>
2019-10-04 09:07:36 -0500

    Created class for code clean up and convinence

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 #Data view configuration
-desired_width=320
-pd.set_option('display.width', desired_width)
-pd.set_option('display.max_columns',13)
+pd.set_option('display.width', 320)
+pd.set_option('display.max_columns', 20)
+#Class just to make things slightly easier to read and handle organize our code...
+class dataFrame():
+    def __init__(self, csv):
+        self.df = pd.read_csv(csv)
+
+    #any methods we need for our data will be here
+    def see_info(self):
+        print(self.df.info())
+
+    def peek(self, peek):
+        print(self.df.head(peek))
+
+    def drop(self, col):
+        self.df = self.df.drop([col], axis=1)
+        return self.df
 
 
-
-
-##Lets start by loading our dataet to see what we are dealing with
-df = pd.read_csv('wineanalysis.csv')
-#print(df.head())
-#We can go ahead and drop Unnamed file since there is no use in this dataset
-df= df.drop(["Unnamed: 0"],axis=1)
-#Lets check it out again...
-print(df.head())
+#Now that we created our class, lets start by opening our dataset and see what columns (Features) we are dealing with
+df = dataFrame('wineanalysis.csv')
+#df.see_info() #We see an col named Unnamed... lets get rid of it.
+df.drop('Unnamed: 0')
+#lets view again...
+#df.see_info()  #Great... lets get rid od this methods here and we can start handeling our selected features
